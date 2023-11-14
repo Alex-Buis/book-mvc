b5e22f1e19165701b665910ec6d109d311764d78
Alex-Buis <alexandre59buisine@gmail.com>
2023-11-14 14:05:39 +0100

    ikdk

--- 06-mvc/views/404.html.php
+++ 06-mvc/views/404.html.php
@@ -1,7 +1,7 @@
 <?php require 'partials/header.html.php' ?>
 
 <div class="max-w-5xl mx-auto px-3">
-    <h1 class="text-3xl text-center mt-8">404, oupsi ! </h1>
+    <h1 class="text-3xl text-center mt-8">404 ! </h1>
 </div>
 
 <?php require 'partials/footer.html.php' ?>
